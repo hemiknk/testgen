40402b1337b7a02e06372ba4be148eb6ea7ed1d5
artem <tema.tarasow@gmail.com>
2016-10-18 10:40:42 +0300

    temp init

--- testgen.php
+++ testgen.php
@@ -10,10 +10,7 @@ php testgen.php controllers or
 php testgen.php models");
 }
 
-// for phar
-if (file_exists(__DIR__.'/vendor/autoload.php')) {
-    require_once(__DIR__.'/vendor/autoload.php');
-} elseif (file_exists(__DIR__.'/../../autoload.php')) {
+if (file_exists(__DIR__.'/../../autoload.php')) {
     require_once __DIR__ . '/../../autoload.php';
 }
 
